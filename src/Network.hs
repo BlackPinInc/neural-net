29a686809d588bd795cfa96683f8201ba3f9f9cc
Sam Gervais <sam@Jordans-New-MacBook-Pro.local>
2016-07-05 19:28:22 -0600

    Stuff

--- src/Network.hs
+++ src/Network.hs
@@ -1,13 +1,35 @@
+{-# LANGUAGE FlexibleContexts #-}
 module Network where
 
 import Data.Matrix
+import Data.Vector (singleton)
 import Data.Maybe (isJust, fromJust)
 
 data Network = Network { numLayers :: Int
                        , sizes :: [Int]
-                       , biases :: Matrix Double
-                       , weights :: Matrix Double
+                       , biases :: [Matrix Double]
+                       , weights :: [Matrix Double]
                        } deriving (Show)
+
+instance Fractional a => Fractional (Matrix a) where
+  (/) = elementwise (/)
+  fromRational = colVector . singleton . fromRational
+
+instance Floating a => Floating (Matrix a) where
+  pi = colVector $ singleton pi
+  exp = fmap exp
+  log = fmap log
+  sin = fmap sin
+  cos = fmap cos
+  asin = fmap asin
+  acos = fmap acos
+  atan = fmap atan
+  sinh = fmap sinh
+  cosh = fmap cosh
+  asinh = fmap asinh
+  acosh = fmap acosh
+  atanh = fmap atanh
+
 -- Thing about what haskell does best.
 -- is it algorithms or equations?
 -- Avoid concrete types, use Num, Floating, or Functor instead of Matrix.
@@ -18,43 +40,31 @@ data Network = Network { numLayers :: Int
 -- The feed forward algorithm has an equation that we can implement directly in Haskell.
 -- No need to follow their code.  Or my own crappy code.  Blind leading the blind.
 --
--- Take a look at the backprop equations(BP1-BP4) I can help you translate these directly 
+-- Take a look at the backprop equations(BP1-BP4) I can help you translate these directly
 -- into haskell functions.  All of these equations have a direct haskell equivalent.
 --
 -- And you should look at this link for how to set up the unit testing.
 -- https://github.com/kazu-yamamoto/unit-test-example
 -- They use HSpec instead of HUnit but I would recomend HSpec because its more haskelly.
 --
--- Lol sorry for all the text I am just very excited about this project and I want you to 
+-- Lol sorry for all the text I am just very excited about this project and I want you to
 -- have a better time on this than I did.
 
-sigmoid :: Floating a => a -> a
-sigmoid z = 1/(1 + exp(-1*z))
+sigmoid :: (Floating (f b), Fractional b, Functor f) => f b -> f b
+sigmoid z = fmap (1/) $ fmap (1+) (exp $ negate z)
 
-sigmoid' :: Floating a => a -> a
-sigmoid' z = sigmoid(z)*(1-sigmoid(z))
+sigmoid' :: Floating c => Matrix c -> Matrix c
+sigmoid' z = elementwise (*) (sigmoid z) (1-sigmoid(z))
 
--- feedForward :: Network -> Matrix Double -> Matrix Double
--- feedForward net a = reshape (last $ sizes net) vectorOut
-  -- where vectorOut = foldl (\a (b,w) -> sigmoid $ w * a + b) (flatten a) $ zip (toColumns $ biases net) (toColumns $ weights net)
+feedForward :: Network -> Matrix Double -> Matrix Double
+feedForward net x
+  | (numLayers net) >= 1 = feedForward newNet a
+  | otherwise            = a
+    where a = sigmoid (elementwise (*) (head $ weights net) x) + (head $ biases net)
+          newNet = Network { numLayers = numLayers net - 1
+                           , sizes = tail (sizes net)
+                           , biases = tail (biases net)
+                           , weights = tail (weights net)}
 
--- sgd :: Network -> [TrainingSet] -> Int -> Int -> Double -> (Maybe [TrainingSet]) -> IO Network
--- sgd net trainingData epochs miniBatchSize eta testData = 1
-  -- where ntest = if isJust testData then fromJust $ length <$> testData else 0
-
--- def SGD(self, training_data, epochs, mini_batch_size, eta,
---             test_data=None):
---         if test_data: n_test = len(test_data)
---         n = len(training_data)
---         for j in xrange(epochs):
---             random.shuffle(training_data)
---             mini_batches = [
---                 training_data[k:k+mini_batch_size]
---                 for k in xrange(0, n, mini_batch_size)]
---             for mini_batch in mini_batches:
---                 self.update_mini_batch(mini_batch, eta)
---             if test_data:
---                 print "Epoch {0}: {1} / {2}".format(
---                     j, self.evaluate(test_data), n_test)
---             else:
---                 print "Epoch {0} complete".format(j)
+cost = 1/2*n*sum(magnitude(output-activation)^2) 
+-- C(w,b)≡1/(2n)∑x ∥ y(x)−a ∥^2.
--- test/NetSpec.hs
+++ test/NetSpec.hs
@@ -2,7 +2,8 @@ module NetSpec (main, spec) where
 
 import Test.Hspec
 import Test.QuickCheck
-
+import Data.Matrix
+import Data.Vector (singleton)
 import Network
 
 -- `main` is here so that this module can be run from GHCi on its own.  It is
@@ -14,23 +15,23 @@ spec :: Spec
 spec = do
   describe "sigmoid" $ do
     it "is 0 at -inf" $ do
-      sigmoid (-1/0 :: Double) `shouldBe` 0
+      sigmoid (fromList 1 1 [-1/0] :: Matrix Double) `shouldBe` 0
     it "is 1 at +inf" $ do
-      sigmoid (1/0 :: Double) `shouldBe` 1
+      sigmoid (fromList 1 1 [1/0] :: Matrix Double) `shouldBe` 1
     it "is 0.5 at 0" $ do
-      sigmoid (0 :: Double) `shouldBe` 0.5
-    it "is between 0 and 1 for all real numbers" $ 
-      property $ \x -> (sigmoid x) <= (1.0 :: Double) && (sigmoid x) >= (0.0 :: Double)
-
+      sigmoid (fromList 1 1 [0] :: Matrix Double) `shouldBe` 0.5
+    -- it "is between 0 and 1 for all real numbers" $
+    --   property $ \x -> (sigmoid x) <= (1.0 :: Matrix Double) && (sigmoid x) >= (0.0 :: Matrix Double)
+    -- it "should work for matrices as well" $ do
+    --   sigmoid (fromList 2 2 [1,1,1,1 :: Double]) == (fromList 2 2 (map sigmoid [1,1,1,1 :: Double]))
+  --
+  --
   describe "sigmoid'" $ do
     it "is 0 at -inf" $ do
-      sigmoid' (-1/0 :: Double) `shouldBe` 0
+      sigmoid' (fromList 1 1 [-1/0] :: Matrix Double) `shouldBe` 0
     it "is 0 at +inf" $ do
-      sigmoid' (1/0 :: Double) `shouldBe` 0
+      sigmoid' (fromList 1 1 [1/0] :: Matrix Double) `shouldBe` 0
     it "is 0.25 at 0" $ do
-      sigmoid' (0 :: Double) `shouldBe` 0.25
-    it "is always positive" $
-      property $ \x -> (sigmoid' x) >= (0.0 :: Double)
-    
-
-
+      sigmoid' (fromList 1 1 [0] :: Matrix Double) `shouldBe` 0.25
+  --   it "is always positive" $
+  --     property $ \x -> (sigmoid' x) >= (0.0 :: Double)

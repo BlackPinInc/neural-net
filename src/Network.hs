8448c15122ec12b834a1bb44b746ca483b8464e1
Jordan Medlock <jordanemedlock@gmail.com>
2016-08-01 11:44:02 -0700

    Changed FullyConnectedLayer to not include the activation layer

--- src/Network.hs
+++ src/Network.hs
@@ -14,8 +14,8 @@ data Sigmoid = Sigmoid deriving Show
 
 instance Activation Sigmoid where
   apply _ = A.map (\z -> 1 / (1 + exp (-z)))
-  delta a x = A.map (\z -> 1 * (1 - z)) s
-    where s = apply a x
+  delta _ = A.map (\z -> let s = 1 / (1 + exp (-z))
+                         in s * (1 - s)) 
 
 
 sigmoid :: (Shape ix) => Acc (Tensor ix) -> Acc (Tensor ix)
@@ -33,9 +33,16 @@ class Layer layer where
   type OutputShape layer :: *
   type WeightShape layer :: *
   type BiasShape layer :: *
-  feedForward :: layer -- ^ Current Layer in the network 
-              -> Acc (Tensor (InputShape layer)) -- ^ Input tensor
-              -> Acc (Tensor (OutputShape layer)) -- ^ Output tensor
+  data InitParams layer :: *
+
+  initialize  :: InitParams layer
+              -> InputShape layer
+              -> OutputShape layer
+              -> layer
+
+  feedForward :: layer
+              -> Acc (Tensor (InputShape layer))
+              -> Acc (Tensor (OutputShape layer))
 
   feedBack    :: layer -- ^ Current Layer in the network
               -> Acc (Tensor (WeightShape layer)) -- ^ The derivative of the previous weight matrix
@@ -56,10 +63,9 @@ class Layer layer where
   cost        :: layer
               -> Float 
   
-data FullyConnectedLayer = forall a . Activation a => FullyConnectedLayer 
+data FullyConnectedLayer = FullyConnectedLayer 
                               { fcl_weights :: Acc (Tensor DIM2)
                               , fcl_biases :: Acc (Tensor DIM1)
-                              , fcl_activation :: a
                               }
 
 instance Layer FullyConnectedLayer where
@@ -67,9 +73,24 @@ instance Layer FullyConnectedLayer where
   type OutputShape FullyConnectedLayer = DIM1
   type WeightShape FullyConnectedLayer = DIM2
   type BiasShape FullyConnectedLayer = DIM1
-  feedForward (FullyConnectedLayer w b a) input = a `apply` z
+  data InitParams FullyConnectedLayer = FCLDefault
+
+  initialize opt inSize outSize = FullyConnectedLayer w b 
+    where (Z:.x) = inSize
+          (Z:.y) = outSize
+          w = fill (lift (Z:.x:.y)) 1 -- TODO: double-check the arrangement of this Mat
+                               -- TODO: Lol also make random instead of just 1
+                               -- I didnt have internet when I wrote this.
+          b = fill (lift (Z:.y)) 1 -- TODO: We might just want to leave this here
+                            -- I have seen people just using 1 for their biases
+                            -- So maybe this is advantagous 
+                            -- Or maybe we pass that as an argument
+                            -- TODO: Look up a program that finds TODOs in files
+                            -- Or maybe make it?
+  feedForward (FullyConnectedLayer w b) input = z
     where wx = multiplyMatrixVector w input
           z = A.zipWith (+) wx b
+
 {-
   feedBack (FullyConnectedLayer w b a) prev = 
     where sp = a `delta` 
--- test/NetSpec.hs
+++ test/NetSpec.hs
@@ -13,49 +13,23 @@ main = hspec spec
 
 spec :: Spec
 spec = do
-  describe "sigmoid" $ do
-    let neginf = unit (-1/0) -- -inf
-    let posinf = unit (1/0) -- +inf
-    let zero = unit 0 -- 0
+  describe "Sigmoid" $ do
+    let neginf = (-1/0) -- -inf
+    let posinf = (1/0) -- +inf
+    let zero = 0 -- 0
+    let testSingleton f x = indexArray (run (f (unit x))) Z
+
     it "is 0 at -inf" $ do
-      indexArray (run (sigmoid neginf)) Z `shouldBe` 0
+      testSingleton (apply Sigmoid) neginf `shouldBe` 0
     it "is 1 at inf" $ do
-      indexArray (run (sigmoid posinf)) Z `shouldBe` 1
+      testSingleton (apply Sigmoid) posinf `shouldBe` 1
     it "is 0.5 at 0" $ do
-      indexArray (run (sigmoid zero)) Z `shouldBe` 0.5
-  {-
-  describe "sigmoid" $ do
-    it "is 0 at -inf" $ do
-      sigmoid (fromList 1 1 [-1/0] :: Matrix Double) `shouldBe` 0
-    it "is 1 at +inf" $ do
-      sigmoid (fromList 1 1 [1/0] :: Matrix Double) `shouldBe` 1
-    it "is 0.5 at 0" $ do
-      sigmoid (fromList 1 1 [0] :: Matrix Double) `shouldBe` 0.5
-    -- it "is between 0 and 1 for all real numbers" $
-    --   property $ \x -> (sigmoid x) <= (1.0 :: Matrix Double) && (sigmoid x) >= (0.0 :: Matrix Double)
-    -- it "should work for matrices as well" $ do
-    --   sigmoid (fromList 2 2 [1,1,1,1 :: Double]) == (fromList 2 2 (map sigmoid [1,1,1,1 :: Double]))
-  --
-  --
-  describe "sigmoid'" $ do
-    it "is 0 at -inf" $ do
-      sigmoid' (fromList 1 1 [-1/0] :: Matrix Double) `shouldBe` 0
-    it "is 0 at +inf" $ do
-      sigmoid' (fromList 1 1 [1/0] :: Matrix Double) `shouldBe` 0
-    it "is 0.25 at 0" $ do
-      sigmoid' (fromList 1 1 [0] :: Matrix Double) `shouldBe` 0.25
-  --   it "is always positive" $
-  --     property $ \x -> (sigmoid' x) >= (0.0 :: Double)
+      testSingleton (apply Sigmoid) zero `shouldBe` 0.5
 
-  -- describe "mkNetwork" $ do
-    -- it "makes a network" $ do
-      -- net <- mkNetwork [1,2,3,6,1] 
-      -- net `shouldSatisfy` (const True)
+    it "has a delta of 0 at -inf" $ do
+      testSingleton (delta Sigmoid) neginf `shouldBe` 0
+    it "has a delta of 0 at inf" $ do
+      testSingleton (delta Sigmoid) posinf `shouldBe` 0
+    it "has a delta of 0.25 at 0" $ do
+      testSingleton (delta Sigmoid) zero `shouldBe` 0.25
 
-    -- it "is 0 at +inf" $ do
-    --   sigmoid' (fromList 1 1 [1/0] :: Matrix Double) `shouldBe` 0
-    -- it "is 0.25 at 0" $ do
-    --   sigmoid' (fromList 1 1 [0] :: Matrix Double) `shouldBe` 0.25
-  --   it "is always positive" $
-  --     property $ \x -> (sigmoid' x) >= (0.0 :: Double)
-  -}
